08851b8e8e2af9d550e5f101355f8955eb03548c
KokiSuesada369 <info.saizen.holdings@gmail.com>
2026-06-02 18:43:49 +0900

    fix next.config.ts conflict

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {}
 
 export default nextConfig
-=======
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
->>>>>>> 39b03e8b167063d71fe053178a03e4e401cf7dd9
